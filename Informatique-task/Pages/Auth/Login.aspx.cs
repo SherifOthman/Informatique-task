7d598645913b0d22c0162a3a7d628d6e553b4fc0
Sheriff Ali <elemlak202099@gmail.com>
2026-05-22 19:45:44 +0300

    Implement login authentication with session

--- Informatique-task/Pages/Auth/Login.aspx.cs
+++ Informatique-task/Pages/Auth/Login.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Informatique_task.Data;
+using Informatique_task.Enums;
+using Informatique_task.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +15,42 @@ namespace Informatique_task.Pages.Auth
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            using(var db = new ApplicationDbContext())
+            {
+                var user = db.Users
+                    .FirstOrDefault(x => x.Username == txtUsername.Text.Trim());
+
+                if(user == null)
+                {
+                    lblError.Text = "Invalid username or password.";
+                    return;
+                }
+
+                bool isValid = PasswordService.Verify(txtPassword.Text, user.PasswordHash);
+
+                if (!isValid)
+                {
+                    lblError.Text = "Invalid username or password.";
+                    return;
+                }
+
+                Session["UserId"] = user.Id;
+                Session["Username"] = user.Id;
+                Session["Role"] = user.Role.ToString();
+
+                if(user.Role == UserRole.Admin)
+                {
+                    Response.Redirect("/Pages/Admin/Dashboard.aspx");
+                }
+                else
+                {
+                    Response.Redirect("~/Pages/Member/MyTasks.aspx");
+                }
+            }
         }
     }
 }
