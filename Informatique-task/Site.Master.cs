2a7870077c66ebb63fb622f48f59a9ac99554a40
Sheriff Ali <elemlak202099@gmail.com>
2026-05-23 00:37:45 +0300

    Highlight active sidebar link with blue background

--- Informatique-task/Site.Master.cs
+++ Informatique-task/Site.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace Informatique_task
 {
@@ -7,6 +8,19 @@ namespace Informatique_task
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string currentPage = Request.Url.AbsolutePath.ToLower();
+            var nav = FindControl("sidebarNav") as Control;
+            if (nav == null) return;
+
+            foreach (Control ctrl in nav.Controls)
+            {
+                if (ctrl is HyperLink link)
+                {
+                    string linkUrl = ResolveUrl(link.NavigateUrl).ToLower();
+                    if (currentPage == linkUrl || currentPage.StartsWith(linkUrl.TrimEnd('/')))
+                        link.CssClass = (link.CssClass + " active").Trim();
+                }
+            }
         }
 
         protected void btnLogout_Click(object sender, EventArgs e)
