d13f0091b87c39968df546b6d13a1912e1170abf
Sheriff Ali <elemlak202099@gmail.com>
2026-05-22 22:00:39 +0300

    Fix navbar URL resolution (use HyperLink), implement logout (clear session), add red background to logout button

--- Informatique-task/Site.Master.cs
+++ Informatique-task/Site.Master.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Informatique_task
 {
-    public partial class Site : System.Web.UI.MasterPage
+    public partial class Site : MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+            Response.Redirect("~/Pages/Auth/Login.aspx");
         }
     }
 }
--- Informatique-task/Site.Master.designer.cs
+++ Informatique-task/Site.Master.designer.cs
@@ -4,5 +4,6 @@
     {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        protected global::System.Web.UI.WebControls.LinkButton btnLogout;
     }
 }
